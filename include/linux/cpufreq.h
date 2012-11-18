7ba0500eda1db67d3ecf6916b5edd043f7b2da37
Lalit Maganti <lalitmaganti@gmail.com>
2012-11-18 12:57:10 -0700

    Fix syntax error
    Change-Id: I6d7ca244d54dfbc6dc1c7b5f249ec61f453bfed4

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -375,7 +375,7 @@ extern struct cpufreq_governor cpufreq_gov_minmax;
 #define CPUFREQ_DEFAULT_GOVERNOR(&cpufreq_gov_minmax)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
 extern struct cpufreq_governor cpufreq_gov_lagfree;
-#define CPUFREQ_DEFAULT_GOVERNOR(&cpufreq_gov_lagfree)
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_lagfree)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
 extern struct cpufreq_governor cpufreq_gov_interactivex;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_interactivex)
