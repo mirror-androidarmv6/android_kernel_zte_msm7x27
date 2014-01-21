f0292a78702a3d198ddea4edff95191b0ceabac1
fagyi <fagyipda@gmail.com>
2014-01-21 16:55:07 +0100

    Revert "zram & zsmalloc: update to msm-3.7 version"
    This reverts commit 6295870b9c30bc630408d3fbc1a0504d6bf5f5f6.

--- drivers/staging/zram/zram_drv.h
+++ drivers/staging/zram/zram_drv.h
@@ -18,13 +18,25 @@
 #include <linux/spinlock.h>
 #include <linux/mutex.h>
 
-#include "../zsmalloc/zsmalloc.h"
+#include "xvmalloc.h"
 
 /*
  * Some arbitrary value. This is just to catch
  * invalid value for num_devices module parameter.
  */
 static const unsigned max_num_devices = 32;
+
+/*
+ * Stored at beginning of each compressed object.
+ *
+ * It stores back-reference to table entry which points to this
+ * object. This is required to support memory defragmentation.
+ */
+struct zobj_header {
+#if 0
+	u32 table_idx;
+#endif
+};
 
 /*-- Configurable parameters */
 
@@ -36,11 +48,10 @@ static const unsigned default_disksize_perc_ram = 25;
  * uncompressed in memory.
  */
 static const u64 max_zpage_size = PAGE_SIZE / 4 * 3;
-static const size_t max_zpage_size = PAGE_SIZE / 4 * 3;
 
 /*
  * NOTE: max_zpage_size must be less than or equal to:
- *   ZS_MAX_ALLOC_SIZE - sizeof(struct zobj_header)
+ *   XV_MAX_ALLOC_SIZE - sizeof(struct zobj_header)
  * otherwise, xv_malloc() would always return failure.
  */
 
@@ -50,13 +61,12 @@ static const size_t max_zpage_size = PAGE_SIZE / 4 * 3;
 #define SECTOR_SIZE		(1 << SECTOR_SHIFT)
 #define SECTORS_PER_PAGE_SHIFT	(PAGE_SHIFT - SECTOR_SHIFT)
 #define SECTORS_PER_PAGE	(1 << SECTORS_PER_PAGE_SHIFT)
-#define ZRAM_LOGICAL_BLOCK_SHIFT 12
-#define ZRAM_LOGICAL_BLOCK_SIZE	(1 << ZRAM_LOGICAL_BLOCK_SHIFT)
-#define ZRAM_SECTOR_PER_LOGICAL_BLOCK	\
-	(1 << (ZRAM_LOGICAL_BLOCK_SHIFT - SECTOR_SHIFT))
 
 /* Flags for zram pages (table[page_no].flags) */
 enum zram_pageflags {
+	/* Page is stored uncompressed */
+	ZRAM_UNCOMPRESSED,
+
 	/* Page consists entirely of zeros */
 	ZRAM_ZERO,
 
@@ -67,11 +77,11 @@ enum zram_pageflags {
 
 /* Allocated for each disk page */
 struct table {
-	unsigned long handle;
-	u16 size;	/* object size (excluding header) */
+	struct page *page;
+	u16 offset;
 	u8 count;	/* object ref count (not yet used) */
 	u8 flags;
-} __aligned(4);
+} __attribute__((aligned(4)));
 
 struct zram_stats {
 	u64 compr_size;		/* compressed size of pages stored */
@@ -81,25 +91,26 @@ struct zram_stats {
 	u64 failed_writes;	/* can happen when memory is too low */
 	u64 invalid_io;		/* non-page-aligned I/O requests */
 	u64 notify_free;	/* no. of swap slot free notifications */
+	u64 discard;		/* no. of block discard callbacks */
 	u32 pages_zero;		/* no. of zero filled pages */
 	u32 pages_stored;	/* no. of pages currently stored */
 	u32 good_compress;	/* % of pages with compression ratio<=50% */
-	u32 bad_compress;	/* % of pages with compression ratio>=75% */
+	u32 pages_expand;	/* % of incompressible pages */
 };
 
 struct zram {
-	struct zs_pool *mem_pool;
+	struct xv_pool *mem_pool;
 	void *compress_workmem;
 	void *compress_buffer;
 	struct table *table;
 	spinlock_t stat64_lock;	/* protect 64-bit stats */
-	struct rw_semaphore lock; /* protect compression buffers and table
-				   * against concurrent read and writes */
+	struct mutex lock;	/* protect compression buffers against
+				 * concurrent writes */
 	struct request_queue *queue;
 	struct gendisk *disk;
 	int init_done;
-	/* Prevent concurrent execution of device init, reset and R/W request */
-	struct rw_semaphore init_lock;
+	/* Prevent concurrent execution of device init and reset */
+	struct mutex init_lock;
 	/*
 	 * This is the limit on amount of *uncompressed* worth of data
 	 * we can store in a disk.
@@ -111,12 +122,11 @@ struct zram {
 
 extern struct zram *zram_devices;
 extern unsigned int zram_num_devices;
-unsigned int zram_get_num_devices(void);
 #ifdef CONFIG_SYSFS
 extern struct attribute_group zram_disk_attr_group;
 #endif
 
 extern int zram_init_device(struct zram *zram);
-extern void __zram_reset_device(struct zram *zram);
+extern void zram_reset_device(struct zram *zram);
 
 #endif
