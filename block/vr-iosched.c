61ebebb3e335d7cb5eb90071388c902ca9ec5f1a
fagyi <fagyipda@gmail.com>
2014-06-25 15:25:23 +0200

    block: Add ROW, VR, Zen I/O schedulers

--- block/vr-iosched.c
+++ block/vr-iosched.c
@@ -72,13 +72,7 @@ return q->elevator->elevator_data;
 static void
 vr_add_rq_rb(struct vr_data *vd, struct request *rq)
 {
-struct request *alias = elv_rb_add(&vd->sort_list, rq);
-
-if (unlikely(alias)) {
-vr_move_request(vd, alias);
-alias = elv_rb_add(&vd->sort_list, rq);
-BUG_ON(alias);
-}
+elv_rb_add(&vd->sort_list, rq);
 
 if (blk_rq_pos(rq) >= vd->last_sector) {
 if (!vd->next_rq || blk_rq_pos(vd->next_rq) > blk_rq_pos(rq))
@@ -305,13 +299,6 @@ vr_move_request(vd, rq);
 return 1;
 }
 
-static int
-vr_queue_empty(struct request_queue *q)
-{
-struct vr_data *vd = vr_get_data(q);
-return RB_EMPTY_ROOT(&vd->sort_list);
-}
-
 static void
 vr_exit_queue(struct elevator_queue *e)
 {
@@ -414,7 +401,6 @@ static struct elevator_type iosched_vr = {
 .elevator_merge_req_fn = vr_merged_requests,
 .elevator_dispatch_fn = vr_dispatch_requests,
 .elevator_add_req_fn = vr_add_request,
-.elevator_queue_empty_fn = vr_queue_empty,
 .elevator_former_req_fn = elv_rb_former_request,
 .elevator_latter_req_fn = elv_rb_latter_request,
 .elevator_init_fn = vr_init_queue,
@@ -444,5 +430,3 @@ module_exit(vr_exit);
 MODULE_AUTHOR("Aaron Carroll");
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("V(R) IO scheduler");
-
-
